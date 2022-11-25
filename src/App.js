cbae7be646982eef4347e2974e2a37fc47881dd5
unknown <llubylinares@gmail.com>
2022-11-25 10:11:06 -0400

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from "./components/Login"
-import "./App.css"
+import "./App.css";
 import { useSelector } from 'react-redux';
 import {selectUser} from "./features/userSlice";
 import Logout from "./components/Logout";
