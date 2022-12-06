e7087ebd22a741a0bc103abe60c1f3fe737e6d43
unknown <llubylinares@gmail.com>
2022-12-05 23:11:30 -0400

    all investors

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit"
 import investorSlice from "./features/investors/investorSlice";
 import userSlice from "./features/user/userSlice";
+import AllInvestorsSlice from "./features/allInvestors/AllInvestorsSlice";
 
 export const store = configureStore({
     reducer: {
         user: userSlice, 
-        investor: investorSlice
+        investor: investorSlice,
+        allInvestors: AllInvestorsSlice
     },
 });
