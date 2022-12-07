daae8f9027ee837a4491edf2adcbbe51d405e272
unknown <llubylinares@gmail.com>
2022-12-07 00:26:45 -0400

    loading

--- src/components/InvestorContainer.js
+++ src/components/InvestorContainer.js
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
+import Investor from './Investor';
+import Wrapper from "../assets/wrappers/InvestorsContainer";
+import { useSelector, useDispatch } from "react-redux";
+import Loading from "./Loading";
 
 const InvestorContainer = () => {
+    const {jobs, isLoading} = useSelector((store) => store.allInvestors);
+    const dispatch= useDispatch();
 
-    return <h2>InvestorContainer</h2>   
+    if (isLoading) {
+        return (
+            <Loading center/>
+        );
+    }
+
+    if (jobs.length === 0){
+        return (
+            <Wrapper>
+                <h2>no investors to display...</h2>
+            </Wrapper>
+        )
+    }
+
+
+   
     
 }
 export default InvestorContainer
